c3b081262161a35d44e9a202a6276de170ecf1b5
akuadane <akuadane1205@gmail.com>
2021-02-28 18:26:25 +0300

    Order update implemented

--- lib/order/screens/order_update_screen.dart
+++ lib/order/screens/order_update_screen.dart
@@ -14,8 +14,6 @@ import '../../item/repository/item_repository.dart';
 class OrderUpdateScreen extends StatefulWidget {
   static String routeName = "/orderUpdateScreen";
   final Order order;
-  final ItemRepository itemRepository =
-      ItemRepository(dataProvider: ItemDataProvider(httpClient: http.Client()));
 
   OrderUpdateScreen(this.order) : assert(order != null);
 
@@ -25,6 +23,8 @@ class OrderUpdateScreen extends StatefulWidget {
 
 class _OrderUpdateScreenState extends State<OrderUpdateScreen> {
   int _amount;
+  final ItemRepository itemRepository =
+      ItemRepository(dataProvider: ItemDataProvider(httpClient: http.Client()));
 
   @override
   void initState() {
@@ -51,98 +51,111 @@ class _OrderUpdateScreenState extends State<OrderUpdateScreen> {
       body: SafeArea(
         child: SingleChildScrollView(
           physics: BouncingScrollPhysics(),
-          child: Container(
-            padding: EdgeInsets.all(15),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SizedBox(
-                  width: double.infinity,
-                ),
-                Text(
-                  "",
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SvgPicture.asset(
-                  "assets/images/burger.svg",
-                  width: MediaQuery.of(context).size.width,
-                  height: 300,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        color: kLoginMainColor,
-                        borderRadius: BorderRadius.circular(10),
+          child: FutureBuilder<Item>(
+            future: itemRepository.getItem(widget.order.itemId),
+            builder: (context, tempItem) {
+              if (tempItem.hasData) {
+                Item item = tempItem.data;
+                return Container(
+                  padding: EdgeInsets.all(15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        width: double.infinity,
                       ),
-                      child: IconButton(
-                        icon: Icon(
-                          Icons.remove,
-                          color: Colors.white,
-                          size: 30,
-                        ),
-                        onPressed: () {
-                          if (_amount > 1) {
-                            setState(() {
-                              _amount--;
-                            });
-                          }
-                        },
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Text(
-                        "${_amount}",
+                      Text(
+                        "${item.name}",
                         style: TextStyle(
                           fontSize: 30,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                        color: kLoginMainColor,
-                        borderRadius: BorderRadius.circular(10),
+                      SvgPicture.asset(
+                        "assets/images/burger.svg",
+                        width: MediaQuery.of(context).size.width,
+                        height: 300,
                       ),
-                      child: IconButton(
-                        icon: Icon(
-                          Icons.add,
-                          color: Colors.white,
-                          size: 30,
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              color: kLoginMainColor,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: IconButton(
+                              icon: Icon(
+                                Icons.remove,
+                                color: Colors.white,
+                                size: 30,
+                              ),
+                              onPressed: () {
+                                if (_amount > 1) {
+                                  setState(() {
+                                    _amount--;
+                                  });
+                                }
+                              },
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(20.0),
+                            child: Text(
+                              "${_amount}",
+                              style: TextStyle(
+                                fontSize: 30,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                              color: kLoginMainColor,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: IconButton(
+                              icon: Icon(
+                                Icons.add,
+                                color: Colors.white,
+                                size: 30,
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  _amount++;
+                                });
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            "Product Description",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30,
+                            ),
+                          ),
+                        ],
+                      ), // This is to align the text to the left
+                      Text(
+                        "${item.description}",
+                        style: TextStyle(
+                          fontSize: 15,
+                          height: 1.5,
                         ),
-                        onPressed: () {
-                          setState(() {
-                            _amount++;
-                          });
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Text(
-                      "Product Description",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 30,
-                      ),
-                    ),
-                  ],
-                ), // This is to align the text to the left
-                Text(
-                  "",
-                  style: TextStyle(
-                    fontSize: 15,
-                    height: 1.5,
+                      )
+                    ],
                   ),
-                )
-              ],
-            ),
+                );
+              }
+              return Container(
+                child: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              );
+            },
           ),
         ),
       ),
